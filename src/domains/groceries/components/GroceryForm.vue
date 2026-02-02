d791046802d247ffd1e2bdc4f3bb5cfc256c93a7
Ilse Arends <ilsearends@live.nl>
2026-02-02 14:06:30 +0100

    Filled grocery form

---
+++ src/domains/groceries/components/GroceryForm.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref } from 'vue'
+
+const props = defineProps(['grocery'])
+const emit = defineEmits(['submit'])
+
+const groceryRef = ref({ ...props.grocery })
+
+const onSubmit = () => {
+    emit('submit', groceryRef.value)
+}
+</script>
+
+
+<template>
+    <form @submit.prevent="onSubmit">
+        <div>
+            <label for="name">Naam:</label>
+            <input v-model="groceryRef.value.name" />
+        </div>
+
+        <div>
+            <label for="quantity">Aantal:</label>
+            <input v-model.number="groceryRef.value.quantity" />
+        </div>
+
+        <button type="submit">Opslaan</button>
+    </form>
+</template>
--- src/domains/groceries/store.js
+++ src/domains/groceries/store.js
@@ -1,7 +1,7 @@
-// State
+
 import { ref } from 'vue'
 import { computed } from 'vue'
-// import GroceriesTable from '../../../../components/GroceriesTable.vue'
+
 const products = ref([
     { name: 'Brood', price: 1.00, quantity: 0 },
     { name: 'Brocoli', price: 0.99, quantity: 0 },
@@ -10,9 +10,9 @@ const products = ref([
 ])
 
 
-// Getters
+
 export const getAllGroceries = computed(() => groceries.value);
 export const getGroceryById = (id) => computed(() => groceries.value.find(grocery => grocery.id == id));
 
-// Actions
+
 export const addGrocery = (grocery) => groceries.value.push(grocery);
